64539a175093d1f57465b8edb78dc17bdc8b6c24
Sem <semia.ladjal@gmail.com>
2015-01-29 14:03:12 +0100

    refactor bad.rb

--- bad.rb
+++ bad.rb
@@ -1,20 +1,13 @@
 class Game
 
+attr_reader :score
+
     def initialize  
         @score = {J1:0, J2:0}
     end 
 
 
     def point!(player)
-
-        if player == :J1
-            @score[:J1] += 1
-        else
-            @score[:J2] += 1
-        end
-    end
-
-    def score
-        @score       
-    end
+        @score[player] += 1       
+    end    
 end
