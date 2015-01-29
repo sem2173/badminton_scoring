887b7c75763603e790c43d52dd7fe7f79ef28f18
Sem <semia.ladjal@gmail.com>
2015-01-29 19:05:57 +0100

    refactor spec file

--- spec/bad_spec.rb
+++ spec/bad_spec.rb
@@ -1,24 +1,30 @@
 require './bad'
 
 describe 'Badminton Score'do
-    let(:game) {game = Game.new}
+
+def check_score(score)
+    new_score = {J1: score[0], J2: score[1]}
+    expect(game.score).to eq(new_score)  
+end
+
+    let(:game) {Game.new}
     it "no begin set no score"do
-    expect(game.score).to eq({J1:0, J2:0})    
+    check_score([0,0])    
     end
 
     it " J1 scores 1 point"do
     game.point!(:J1)
-    expect(game.score).to eq({J1:1, J2:0})    
+    check_score([1,0])    
     end
 
     it " J2 scores 1 point"do
     game.point!(:J2)
-    expect(game.score).to eq({J1:0, J2:1})
+     check_score([0,1])
     end 
 
     it " J1 scores 1 point and J2 scores 1 point"do
     game.point!(:J1)
     game.point!(:J2)
-    expect(game.score).to eq({J1:1, J2:1})
+    check_score([1,1])
     end
 end
