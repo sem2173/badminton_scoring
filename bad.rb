f9dc8a2d0277bc03bc8aa07fe7fed1f46c6696dc
Sem <semia.ladjal@gmail.com>
2015-02-04 17:18:31 +0100

    add other player

--- bad.rb
+++ bad.rb
@@ -10,9 +10,13 @@ class Game
     def point!(player)        
         @point[player] += 1
 
-        if @point[player] == 21
+        if @point[player] == 21 && @point[player] - @point[other_player(player)] >= 2  
             @point = {J1:0, J2:0}
-            @set[player]+=1
+            @set[player] += 1
         end
+    end
+
+    def other_player(player)
+        player == :J2 ? :J1 : :J2       
     end   
 end
--- spec/bad_spec.rb
+++ spec/bad_spec.rb
@@ -1,6 +1,6 @@
 require './bad'
 
-describe 'Badminton Score'do
+describe 'Badminton Score' do
 
     def check_point(score)
         new_score = {J1: score[0], J2: score[1]}
@@ -9,30 +9,42 @@ describe 'Badminton Score'do
 
     let(:game) {Game.new}
     
-    it "no begin set no score"do
+    it "no begin set no score" do
         check_point([0,0])    
     end
 
-    it " J1 scores 1 point"do
+    it " J1 scores 1 point" do
         game.point!(:J1)
         check_point([1,0])    
     end
 
-    it " J2 scores 1 point"do
+    it " J2 scores 1 point" do
         game.point!(:J2)
         check_point([0,1])
     end 
 
-    it " J1 scores 1 point and J2 scores 1 point"do
+    it " J1 scores 1 point and J2 scores 1 point" do
         game.point!(:J1)
         game.point!(:J2)
         check_point([1,1])
     end
 
-    it " J1 win a set"do
+    it " J1 win a set" do
         21.times { game.point!(:J1) }
         new_set = {J1:1, J2: 0}
         expect(game.set).to eq(new_set)
         check_point([0,0])
+    end
+
+    it "2 points difference" do
+        20.times {game.point!(:J2)}
+        21.times {game.point!(:J1)}
+        expect(game.set).to eq({J1:0, J2: 0})
+        check_point([21,20])
+    end
+
+    it "other player" do
+        expect(game.other_player(:J1)).to eq(:J2)
+        expect(game.other_player(:J2)).to eq(:J1)
     end
 end
