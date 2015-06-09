5141663db8b03ec2c604bcad67d451efa6359b74
Sem <semia.ladjal@gmail.com>
2015-06-09 16:39:04 +0200

    add @data

--- app.rb
+++ app.rb
@@ -1,19 +1,19 @@
+require 'json'
 require 'sinatra'
 require './lib/game.rb'
-require 'json'
 
 get '/' do
    game = Game.new
    @point = game.point
+   @data = JSON.parse(File.read("data.json"))
    erb :index
 end
 
 post '/' do
     game = Game.new
-    count = game.point!(params[:marque].gsub(/oueur /, '').to_sym)
+    game.point!(params[:marque].gsub(/oueur /, '').to_sym)
     @point = game.point
-    values = JSON.parse(@point.to_json)
-    File.open('data.json', 'w'){|file| file.write(values)}
+    File.write("data.json", game.point.to_json)
     erb :index
 
 end
